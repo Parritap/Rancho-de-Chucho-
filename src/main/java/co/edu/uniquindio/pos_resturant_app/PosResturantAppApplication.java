8e361e49312b2cfe69e6ad0302b34bdf108c0c3f
Parritap <juane.parrap@uqvirtual.edu.co>
2025-02-17 10:52:25 -0500

    .

--- src/main/java/co/edu/uniquindio/pos_resturant_app/PosResturantAppApplication.java
+++ src/main/java/co/edu/uniquindio/pos_resturant_app/PosResturantAppApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-//@EntityScan("co.edu.uniquindio.pos_resturant_app.entities")
 public class PosResturantAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PosResturantAppApplication.class, args);
     }
-
 }
